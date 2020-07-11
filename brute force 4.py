da882a52b1cd69e6022e022c396770468e136e47
Mirae <26future@hanmail.net>
2020-07-11 14:16:00 +0900

    200711 Baekjoon 1018

--- brute force 4.py
+++ brute force 4.py
@@ -8,47 +8,43 @@ for i in range(N): # 전체 보드 패턴
     square.append(colors)
 
 chess_boards = [] # 잘라낸 체스판
-for n in range(N-7):b
-    for m in range(M-7):
-        chess_board = square[n:n+8][m:m+8]
-        chess_boards.append(chess_board)
+for n in range(N-7): # 행 슬라이싱
+    square_row = square[n:n+8]
 
-# chess_boards = [['WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBBBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW']]
+    for m in range(M-7): # 열 슬라이싱
+        board=[]
 
-
+        for row in square_row:
+            square_column = row[m:m+8]
+            board.append(square_column)
+        chess_boards.append(board)
 
 counts = [] # 다시 칠해야 하는 개수
 for board in chess_boards:
-    print(board)
 
-#     # 첫번째 칸이 W인 경우
-#     count1 = 0
-#     for i in range(8):
-#         for j in range(8):
-#             if (i+j) % 2 != 0 and board[i][j] != 'B': # 합이 홀수 : B
-#                 count1 += 1
-#             elif (i+j) % 2 ==0 and board[i][j] != 'W': # 합이 짝수 : W
-#                 count1 += 1
-#             else:
-#                 pass
-#     counts.append(count1)
-#
-#
-#
-#
-#     # 첫번째 칸이 B인 경우
-#     count2 = 0
-#     for i in range(8):
-#         for j in range(8):
-#             if (i+j) % 2 == 0 and board[i][j] != 'B': # 합이 짝수 : B
-#                 count2 += 1
-#             elif (i+j) % 2 !=0 and board[i][j] != 'W': # 합이 홀수: W
-#                 count2 += 1
-#             else:
-#                 pass
-#     counts.append(count2)
-#
-#
-#
-# print(min(counts))
-#
+    # 첫번째 칸이 W인 경우
+    count1 = 0
+    for i in range(8):
+        for j in range(8):
+            if (i+j) % 2 != 0 and board[i][j] != 'B': # 합이 홀수 : B
+                count1 += 1
+            elif (i+j) % 2 ==0 and board[i][j] != 'W': # 합이 짝수 : W
+                count1 += 1
+            else:
+                pass
+    counts.append(count1)
+
+    # 첫번째 칸이 B인 경우
+    count2 = 0
+    for i in range(8):
+        for j in range(8):
+            if (i+j) % 2 == 0 and board[i][j] != 'B': # 합이 짝수 : B
+                count2 += 1
+            elif (i+j) % 2 !=0 and board[i][j] != 'W': # 합이 홀수: W
+                count2 += 1
+            else:
+                pass
+    counts.append(count2)
+
+print(min(counts))
+
