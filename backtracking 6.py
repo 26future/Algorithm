93850d186c9fa83f9974e79de7cc1101c15fd0ee
Mirae <26future@hanmail.net>
2020-07-21 17:51:38 +0900

    200721 Baekjoon 2580 Sudoku

--- backtracking 6.py
+++ backtracking 6.py
@@ -64,27 +64,64 @@ def check_column(boards_column):
     check_column(boards_column)
 
 # 3*3 구조로 재배치
-boards_square = []
-list(range())
-square = []
-for i in range(3): # 행 인덱스
-    for j in range(3): # 열 인덱스
-        square.append(new_boards[i][j])
-boards_square.append(square)
+# square에 따른 리스트 재배치
+global square_range
+square_range = [list(range(0, 3)), list(range(3, 6)), list(range(6, 9))]
 
-square = []
-for i in range(3,6):
-    for j in range(3,6):
+def make_boards_square(new_boards):
+    boards_square = []
+    for row_idx in square_range: # 행 인덱스
+        for column_idx in square_range: # 열 인덱스
+            square = []
+            for i in row_idx:
+                for j in column_idx:
+                    square.append(new_boards[i][j])
+            boards_square.append(square)
+    return boards_square
 
 
+# 각 square에서 빈 칸이 하나인 경우
+def check_square(boards_square):
+    # 빈 칸이 하나인 square가 더이상 없는 경우
+    cnt = 0
+    for square in boards_square:
+        if square.count(0) == 1:
+            cnt += 1
+    if cnt == 0:
+        print('b:',boards_square)
+
+        # 행에 따른 리스트 재배치
+        squares = [boards_square[0:3], boards_square[3:6], boards_square[6:9]]
+        new_boards = []
+
+        for square in squares:
+            for column_idx in square_range:
+                row = []
+                for sq in square:
+                    for j in column_idx:
+                        row.append(sq[j])
+                new_boards.append(row)
+        print(new_boards)
+        return new_boards
+
+    # 빈 칸이 하나인 square가 존재하는 경우
+    for square in boards_square:
+        if square.count(0) == 1:
+            for number in range(1, 10):
+                if number not in square:
+                    square[square.index(0)] = number
+
+    check_square(boards_square)
 
 
+check_row(new_boards)
+print(new_boards)
+boards_column = make_boards_column(new_boards)
+print(boards_column) # 열별 리스트로 재배치
+check_column(boards_column)
+print(new_boards) # 행별 리스트로 재배치
 
-
-# check_row(new_boards)
-# print(new_boards)
-# boards_column = make_boards_column(new_boards)
-# print(boards_column) # 열별 리스트로 재배치
-# check_column(boards_column)
-# print(new_boards) # 행별 리스트로 재배치
-
+boards_square = make_boards_square(new_boards)
+print(boards_square) # square별 리스트로 재배치
+check_square(boards_square)
+print(new_boards)
