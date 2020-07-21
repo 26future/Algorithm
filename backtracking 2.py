9116669d08432ab18072bebeda1403b6a0c9b49e
Mirae <26future@hanmail.net>
2020-07-21 18:17:03 +0900

    200714 Baekjoon 15650

---
+++ backtracking 2.py
@@ -0,0 +1,13 @@
+import itertools
+
+numbers = input().split()
+N = int(numbers[0]) # N까지의 수
+M = int(numbers[1]) # 수열의 길이
+
+numbers = [n for n in range(1,N+1)]
+
+from itertools import combinations
+sequences = list(combinations(numbers,M))
+
+for sequence in sequences:
+    print(str(sequence)[1:-1].replace(',',''))
