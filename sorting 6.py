05cd1906e1bcad04741e92d3c25a6269e3b51aa9
Mirae <26future@hanmail.net>
2020-07-28 19:08:50 +0900

    200728 Baekjoon 11650

---
+++ sorting 6.py
@@ -0,0 +1,10 @@
+import sys
+
+N = int(input())
+numbers = [sys.stdin.readline().split() for i in range(N)]
+
+coordinates = list(map(lambda x:(int(x[0]),int(x[1])), numbers))
+coordinates.sort(key=lambda x:(x[0],x[1]))
+
+for c in coordinates:
+    print(c[0],c[1])
