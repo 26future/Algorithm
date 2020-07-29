e11a3ac8f0fddb1bee54714c866d409fe8efcf35
Mirae <26future@hanmail.net>
2020-07-29 18:15:24 +0900

    200729 Baekjoon 10989 Memory Limit Exceeded

--- sorting 3.py
+++ sorting 3.py
@@ -1,9 +1,14 @@
 import sys
+from collections import Counter
 
 N = int(input())
 numbers = [int(sys.stdin.readline().split()[0]) for i in range(N)]
 
+cnt_numbers = dict(Counter(numbers))
 
-for num in list(set(numbers)):
-    for j in range(numbers.count(num)):
-        print(num)
+# 개수 세기
+s_numbers = sorted(cnt_numbers.items())
+
+for num in s_numbers:
+    for j in range(num[1]):
+        print(num[0])
