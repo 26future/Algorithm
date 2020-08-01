e4c636c1455c6a6a4b068729814176f251682fcd
Mirae <26future@hanmail.net>
2020-08-01 16:27:15 +0900

    200801 Baekjoon 1904 Memory Limit Exceeded

--- dynamic programming 3.py
+++ dynamic programming 3.py
@@ -1,14 +1,11 @@
 import sys
 N = int(sys.stdin.readline().strip())
 
-count = [1,2]
-
-def count_tile(n):
-    n_cnt = count[n-2]*2-1
-    count.append(n_cnt)
-    return
+count = [0]*1000001
+count[0] = 1
+count[1] = 2
 
 for n in range(3,N+1):
-    count_tile(n)
+    count[n-1] = 2*count[n-2]-1
 
-print(count[N-1]%15746)
+print(count[n-1]%15746)
