4d06b0120d26ee44536426bc35aee6f3da695528
Mirae <26future@hanmail.net>
2021-01-20 22:11:25 +0900

    210120 Baekjoon Memory Limit Exceeded

---
+++ dynamic programming 9-1.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+N = int(sys.stdin.readline())
+numbers = list(map(str, list(range(1,10))))
+
+def stair_numbers(numbers):
+    new = []
+    for n in numbers:
+        if n[-1] == '9':
+            new.append(n+'8')
+        elif n[-1] == '0':
+            new.append(n+'1')
+        else:
+            new.append(n + str(int(n[-1])-1))
+            new.append(n + str(int(n[-1])+1))
+
+    del numbers
+    numbers = new[:]
+    del new
+    return numbers
+
+
+for i in range(N-1):
+    numbers = stair_numbers(numbers)
+
+print(divmod(len(numbers),1000000000)[1])
