c562cc99688e31821529a2da816380df724bdf51
26future <58538283+26future@users.noreply.github.com>
2021-03-10 01:13:27 +0900

    210309 Programmers level1

---
+++ no same numbers.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = [arr[0]]
+    arr = arr[1:]
+
+    for n in arr:
+        if n != answer[-1]:
+            answer.append(n)
+        else:
+            pass
+
+    return answer
